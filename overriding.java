ad46e5b91bf6ea3afde3a7365f402cbac457edc2
pranaya205 <81561049+pranaya205@users.noreply.github.com>
2021-03-29 17:51:07 +0530

    Add files via upload

---
+++ overriding.java
@@ -0,0 +1,22 @@
+class X
+{
+public void display()
+{
+System.out.println("class X");
+}
+}
+class Y extends X
+{
+public void display()
+{
+System.out.prinln("class Y");
+}
+}
+class Overriding
+{
+public static void main(String a[])
+{
+X X1=new;
+X1.display();
+}
+}
