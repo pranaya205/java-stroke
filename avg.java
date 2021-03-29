3c02482c5226b63a864158c572b579658905c0bb
pranaya205 <81561049+pranaya205@users.noreply.github.com>
2021-03-29 17:18:58 +0530

    Add files via upload

---
+++ avg.java
@@ -0,0 +1,18 @@
+class avg
+{
+public static void main(String arg[])
+{
+int a[]=newint[n];
+float a[0],a[1],a[2],a[3],a[4];
+a[0]=10;
+a[1]=20;
+a[2]=30;
+a[3]=40;
+a[4]=50;
+
+avg=avg+a[i];
+System.out.println("average marks ");
+}
+
+System.out.println("highest marks");
+}
