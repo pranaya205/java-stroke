d1c9099616f009a8320df9313d4163810dcada2a
pranaya205 <81561049+pranaya205@users.noreply.github.com>
2021-03-29 17:20:33 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,13 @@
+class circle
+{
+public static void main(String a[])
+{
+int radius =10;
+final float PI=3.14f;
+float area=PI*radius*radius;
+float perimeter=2*PI*radius;
+System.out.println("area of circle ="+area);
+System.out.println("perimeter of circle="+perimeter);
+}
+}
+
